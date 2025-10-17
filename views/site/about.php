b8edc81984e6eb0b2895e8b45a08fe91777d6c5f
nmrsilapena-ctrl <nm.rsilapena@gmail.com>
2025-10-17 14:58:26 +0800

    Try lang

--- views/site/about.php
+++ views/site/about.php
@@ -4,14 +4,14 @@
 
 use yii\helpers\Html;
 
-$this->title = 'About';
+$this->title = 'About hello';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        This is the About page. You may modify the following file to customize its content:
+        This is the About Reymark page. You may modify the following file to customize its content:
     </p>
 
     <code><?= __FILE__ ?></code>
